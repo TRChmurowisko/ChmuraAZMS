6a6d5202dc1a9dc0533d0d8f028b0ba34a627fc5
Tomasz Romanowski <tromanowski@objectivity.co.uk>
2018-11-06 14:39:01 +0100

    sender completed

--- Week6/QueueSender/QueueTimer.cs
+++ Week6/QueueSender/QueueTimer.cs
@@ -1,16 +1,25 @@
 using System;
+
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+// using Microsoft.WindowsAzure.Storage; 
+// using Microsoft.WindowsAzure.Storage.Queue; 
 
 namespace TR.Test
 {
     public static class QueueTimer
     {
         [FunctionName("QueueTimer")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, 
+                        ILogger log,
+                        [Queue("stq-test",Connection = "AzureWebJobsStorage")] 
+                        out string queueMessage)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            queueMessage = $"New message created at {DateTime.UtcNow.ToString()} utc";
+            log.LogInformation($"C# Message created at: {DateTime.Now}");
         }
     }
 }
